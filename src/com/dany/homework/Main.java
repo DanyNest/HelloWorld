a1ba7b718579eaa1068518da669d65548060b6fb
Dany Nesterov <nesterov1200997@gmail.com>
2021-10-13 23:12:55 +0200

    second commit

--- src/com/dany/homework/Main.java
+++ src/com/dany/homework/Main.java
@@ -1,7 +1,7 @@
 package com.dany.homework;
 
-public class Main {
+public class  Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, Mates!");
     }
 }
